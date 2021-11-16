9d3784d38e0f5322b7319470c0d52f8ab395c35d
AmaliaMaria25 <amalialungocea@yahoo.com>
2021-11-16 18:42:31 +0200

    Added dependencies, http request and method beans

---
+++ src/main/java/HTTP/HttpMethod.java
@@ -0,0 +1,17 @@
+package HTTP;
+
+
+public enum HttpMethod {
+
+    GET, HEAD;
+    public static final int MAX_LENGTH;
+    static{
+        int maxLength = -1;
+        for(HttpMethod method : values()){
+            if(method.name().length() > maxLength){
+                maxLength = method.name().length();
+            }
+        }
+        MAX_LENGTH = maxLength;
+    }
+}
---
+++ src/main/java/HTTP/Request.java
@@ -0,0 +1,60 @@
+package HTTP;
+
+import com.sun.org.slf4j.internal.Logger;
+import com.sun.org.slf4j.internal.LoggerFactory;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class Request {
+
+    private final static Logger LOGGER  = LoggerFactory.getLogger(Request.class);
+    private String URI;
+    private HttpMethod method;
+    private String version;
+    private ArrayList<String> headers;
+
+    public Request(InputStream input) {
+        InputStreamReader inputStreamReader = new InputStreamReader(input);
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+        
+        try {
+            parseRequest(bufferedReader);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public void parseRequest(BufferedReader input) throws IOException {
+        StringBuilder stringBuilder = new StringBuilder();
+        String line;
+        while (!(line = input.readLine()).isEmpty()) {
+            stringBuilder.append(line + "\r\n");
+        }
+        String requestString = stringBuilder.toString();
+        LOGGER.debug("/*/*" + requestString);
+    }
+
+
+    public String getURI() { return URI; }
+
+    public void setURI(String URI) { this.URI = URI; }
+
+    public HttpMethod getMethod() { return method; }
+
+    public void setMethod(HttpMethod method) { this.method = method; }
+
+    public String getVersion() { return version; }
+
+    public void setVersion(String version) { this.version = version; }
+
+    public ArrayList<String> getHeaders() { return headers; }
+
+    public void setHeaders(ArrayList<String> headers) { this.headers = headers; }
+
+
+}
